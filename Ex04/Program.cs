ec2ceb4fc0048a1cec72e35a00b2fa5798dcffa6
staicu adrian <adrian_staicu88@yahoo.com>
2020-02-11 22:48:25 +0200

    Add project files.

---
+++ Ex04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string value = "Curs de C#";
+            char[] array = value.ToCharArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+            
+                char letter = array[i];
+           
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
